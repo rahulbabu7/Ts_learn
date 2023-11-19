d2b8803175ac55f1330ed9f4d91f81d49fe218b9
rahulbabu7 <rahulbabu436@gmail.com>
2023-11-19 09:52:56 +0530

    addded large number

--- dist/index.js
+++ dist/index.js
@@ -5,4 +5,6 @@ if (age < 50) {
     age += 10;
 }
 console.log(age);
+let sales = 123456789;
+console.log(sales);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,5 @@ if(age < 50)
     age +=10;
 }
 console.log(age);
+let sales: number= 123_4567_89;  //added underscore to show it is a large number .More readable
+console.log(sales);
