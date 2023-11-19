14aff5adeb34110566512dbb592c4c8df8782645
rahulbabu7 <rahulbabu436@gmail.com>
2023-11-19 10:04:26 +0530

    update

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,21 @@ if(age < 50)
 console.log(age);
 let sales: number= 123_4567_89;  //added underscore to show it is a large number .More readable
 console.log(sales);
+let level ;  //type of any
+
+//type of any is not goog because
+level = 1;
+level = "Rahul";
+
+//type any can be used to be like any data type
+
+
+// function render(document) //!here the parameter is implicityly set to any type
+// {
+//     console.log(document);
+// }
+
+let isPusblished: boolean = true;
+
+
+//!array
